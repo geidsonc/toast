efa213ca9771e6a4e2e78df26816737afc8502da
Leandro Gonçalves <leandrow01s@gmail.com>
2017-10-26 15:24:16 -0200

    Remove unnecessary classes

--- src/js/toast.js
+++ src/js/toast.js
@@ -7,10 +7,10 @@ $(function() {
 
 		$("#toast-container").show(0);
 
-		var html = '<div id="' + id + '" class="toast-alert toast flexbox z-depth-1">'+
-				'<div class="box flex-grow-2 toast-message">' + m + '</div>'+
-				'<div class="box toast-close">'+
-					'<svg fill="#ffffff" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">'+
+		var html = '<div id="' + id + '"class="toast">'+
+				'<div class="toast-message">' + m + '</div>'+
+				'<div class="toast-close">'+
+					'<svg fill="#ffffff" height="22" viewBox="0 0 24 24" width="22" xmlns="http://www.w3.org/2000/svg">'+
 						'<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>'+
 						'<path d="M0 0h24v24H0z" fill="none"/>'+
 					'</svg>'+
